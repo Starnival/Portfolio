733881c26129d1bbe25935030d86373a9b838d65
Starnival <starnival@gmail.com>
2024-04-16 03:09:18 -0400

    Version 1.0

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Josue Noriega - Portfolio';
-export const SITE_DESCRIPTION = 'Personal portfolio of the developer.';
+export const SITE_DESCRIPTION = 'Personal portfolio of the developer';
